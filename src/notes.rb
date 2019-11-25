0e35555781f4d6e84d706836a2a9c85554d778e1
Andy Meneely <andy@se.rit.edu>
2019-11-25 00:15:47 -0500

    notes download tweaks

--- src/notes.rb
+++ src/notes.rb
@@ -1,5 +1,9 @@
 require 'open-uri'
 
 # Save our notes from SimpleNote
+print "Saving notes from SimpleNote..."
 html = open('https://app.simplenote.com/p/ggPlM7').read
-File.open('brainstorming.html', 'w') { |f| f.write html }
+File.open('brainstorming.html', 'w+') do |f|
+  f.write html
+end
+puts "done!"
