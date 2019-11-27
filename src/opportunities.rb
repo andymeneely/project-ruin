2b38c375698298fdad4b7100426b471046d6fb62
Andy Meneely <andy@se.rit.edu>
2019-11-26 19:45:41 -0500

    working on more flexible cards

--- src/opportunities.rb
+++ src/opportunities.rb
@@ -11,9 +11,9 @@ Squib::Deck.new(cards: data.nrows) do
 
   text str: data.name, layout: :name
 
-  svg file: 'bw/opportunity.svg', width: :deck, height: :deck
+  # svg file: 'bw/opportunity.svg', width: :deck, height: :deck
 
-  %w(Consume1 Consume2 Required1 Required2).each do |col|
+  %w(Consume1 Consume2 Store1 Store2 Required1 Required2).each do |col|
     data["#{col}SVG"] = data["#{col}Icon"].map do |icon|
       unless icon.to_s.empty?
         GameIcons.get(icon).recolor(fg: '000', bg_opacity: 0).string
@@ -21,10 +21,16 @@ Squib::Deck.new(cards: data.nrows) do
     end
     svg data: data["#{col}SVG"], layout: "#{col}Icon"
     text str: data[col], layout: col
+    # Draw these UI frames only if the column is not nil
+    puts data[col].map { |x| x.to_s.empty? ? nil : "#{col}Frame" }
+    rect layout: data[col].map { |x| x.to_s.empty? ? nil : "#{col}Frame" }
   end
 
-  text str: data.action1, layout: :Action1
-  text str: data.tags, layout: :tags
+  # text layout: data.consume1.map { |x| x.nil? ? nil : :Consume1Arrow }
+  # text layout: data.consume2.map { |x| x.nil? ? nil : :Consume2Arrow }
+
+  # text str: data.action1, layout: :Action1
+  # text str: data.tags, layout: :tags
 
   text str: ProjectRuin::VERSION, layout: :version
 
