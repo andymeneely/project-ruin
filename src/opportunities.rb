6adaba0d4d6ab11c93fd1280f8f98c8480ba4542
Andy Meneely <andy@se.rit.edu>
2019-12-02 00:45:19 -0500

    more conversions to new formats

--- src/opportunities.rb
+++ src/opportunities.rb
@@ -30,38 +30,46 @@ Squib::Deck.new(cards: data.nrows) do
 
   text str: data.name, layout: :name
 
-
   # Draw these UI frames only if the column is not nil
   # UGH. Looks like a bug in Squib. svg's layout isn't properly supporting expansion
-  text str: data["Consume1"], layout: :Consume1
-  svg layout: "Consume1Frame",
-      file: data["Consume1Resource"].map { |r| r.to_s.empty? ? nil : 'bw/consume.svg' }
-  svg data: data["Consume1Resource"].map { |r| icon[r] },
+  text str: data.consume1, layout: :Consume1
+  text str: data.consume1resource, layout: :Consume1ResourceText
+  svg layout: :Consume1Frame,
+      file: data.consume1resource.map { |r| r.to_s.empty? ? nil : 'bw/consume.svg' }
+  svg data: data.consume1resource.map { |r| icon[r] },
       layout: :Consume1Resource
-  text str: data["Consume2"], layout: :Consume2
-  svg layout: "Consume2Frame",
-      file: data["Consume2Resource"].map { |r| r.to_s.empty? ? nil : 'bw/consume.svg' }
-  svg data: data["Consume2Resource"].map { |r| icon[r] },
+
+  text str: data.consume2, layout: :Consume2
+  text str: data.consume2resource, layout: :Consume2ResourceText
+  svg layout: :Consume2Frame,
+      file: data.consume2resource.map { |r| r.to_s.empty? ? nil : 'bw/consume.svg' }
+  svg data: data.consume2resource.map { |r| icon[r] },
       layout: :Consume2Resource
 
-  text str: data["Store1"], layout: :Store1
-  svg layout: "Store1Frame",
-      file: data["Store1Resource"].map { |r| r.to_s.empty? ? nil : 'bw/store.svg' }
-  svg data: data["Store1Resource"].map { |r| icon[r] }, layout: :Store1Resource
-  text str: data["Store2"], layout: :Store2
-  svg layout: "Store2Frame",
-      file: data["Store2Resource"].map { |r| r.to_s.empty? ? nil : 'bw/store.svg' }
-  svg data: data["Store2Resource"].map { |r| icon[r] }, layout: :Store2Resource
+  text str: data.store1, layout: :Store1
+  text str: data.store1resource, layout: :Store1ResourceText
+  svg layout: :Store1Frame,
+      file: data.store1resource.map { |r| r.to_s.empty? ? nil : 'bw/store.svg' }
+  svg data: data.store1resource.map { |r| icon[r] }, layout: :Store1Resource
 
-  text str: data["Required1"], layout: :Required1
-  svg layout: "Required1Frame",
-      file: data["Required1Resource"].map { |r| r.to_s.empty? ? nil : 'bw/store.svg' }
-  svg data: data["Required1Resource"].map { |r| icon[r] },
+  text str: data.store2, layout: :Store2
+  text str: data.store2resource, layout: :Store2ResourceText
+  svg layout: :Store2Frame,
+      file: data.store2resource.map { |r| r.to_s.empty? ? nil : 'bw/store.svg' }
+  svg data: data.store2resource.map { |r| icon[r] }, layout: :Store2Resource
+
+  text str: data.required1, layout: :Required1
+  text str: data.required1resource, layout: :Required1ResourceText
+  svg layout: :Required1Frame,
+      file: data.required1resource.map { |r| r.to_s.empty? ? nil : 'bw/required.svg' }
+  svg data: data.required1resource.map { |r| icon[r] },
       layout: :Required1Resource
-  text str: data["Required2"], layout: :Required2
-  svg layout: "Required2Frame",
-      file: data["Required2Resource"].map { |r| r.to_s.empty? ? nil : 'bw/store.svg' }
-  svg data: data["Required2Resource"].map { |r| icon[r] },
+
+  text str: data.required2, layout: :Required2
+  text str: data.required2resource, layout: :Required2ResourceText
+  svg layout: :Required2Frame,
+      file: data.required2resource.map { |r| r.to_s.empty? ? nil : 'bw/required.svg' }
+  svg data: data.required2resource.map { |r| icon[r] },
       layout: :Required2Resource
 
   %w(Action1 Action2 Action3).each do |col|
@@ -87,9 +95,14 @@ Squib::Deck.new(cards: data.nrows) do
   save_png prefix: 'opportunity_preview_',
            trim: '0.125in', trim_radius: '0.125in'
 
+  build :pdf do
+   save_pdf file: 'opportunities.pdf'
+  end
+
   cut_zone
   save_sheet prefix: 'opportunity_sheet_widescreen_', columns: 12,
              trim: '0.125in', trim_radius: '0.125in'
   save_sheet prefix: 'opportunity_sheet_portrait_', columns: 2,
              trim: '0.125in', trim_radius: '0.125in'
+  
 end
